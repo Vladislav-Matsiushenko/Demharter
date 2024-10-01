d422be4a85f74f3040fa1abb5d1c00db3851c612
Vladislav-Matsiushenko <vladislav.matsiushenko@softera.by>
2024-10-01 11:57:13 +0300

    Refactored deleting products

--- custom/plugins/MagediaDemharter/Commands/DeleteProductsCommand.php
+++ custom/plugins/MagediaDemharter/Commands/DeleteProductsCommand.php
@@ -5,13 +5,13 @@ namespace MagediaDemharter\Commands;
 use Shopware\Commands\ShopwareCommand;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
-use MagediaDemharter\Service\DeleteProducts;
+use MagediaDemharter\Service\DeleteProductsService;
 
 class DeleteProductsCommand extends ShopwareCommand
 {
     private $deleteProductsService;
 
-    public function __construct(DeleteProducts $deleteProductsService)
+    public function __construct(DeleteProductsService $deleteProductsService)
     {
         parent::__construct();
         $this->deleteProductsService = $deleteProductsService;
@@ -21,14 +21,12 @@ class DeleteProductsCommand extends ShopwareCommand
     {
         $this
             ->setName('demharter:delete_products')
-            ->setDescription('Delete Products (demharter)');
+            ->setDescription('Delete Products (Demharter)');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $this->deleteProductsService->deleteProductsFromCsv();
-        $output->writeln("\n");
-        $output->writeln('Success');
+        $this->deleteProductsService->execute();
+        $output->writeln("\nSuccess");
     }
-
 }
--- custom/plugins/MagediaDemharter/Service/DeleteProductsService.php
+++ custom/plugins/MagediaDemharter/Service/DeleteProductsService.php
@@ -2,12 +2,21 @@
 
 namespace MagediaDemharter\Service;
 
-class DeleteProducts
+class DeleteProductsService
 {
+// Local
     private $logFilePath = '/var/www/quad-ersatzteile.loc/NotDeletedProducts.txt';
     private $ebayPricesFilePath = '/var/www/quad-ersatzteile.loc/EbayPrices.txt';
     private $csvFilePath = '/var/www/quad-ersatzteile.loc/Teileexport-grp-3.csv';
     private $endpointUrl = 'http://quad-ersatzteile.loc/api';
+
+// Staging
+//    private $logFilePath = '/usr/home/mipzhm/public_html/staging/NotDeletedProducts.txt';
+//    private $ebayPricesFilePath = '/usr/home/mipzhm/public_html/staging/EbayPrices.txt';
+//    private $csvFilePath = '/usr/home/mipzhm/public_html/staging/Teileexport-grp-3.csv';
+//    private $endpointUrl = 'http://staging.quad-ersatzteile.com/api';
+
+// Live
 //    private $logFilePath = '/usr/home/mipzhm/public_html/NotDeletedProducts.txt';
 //    private $ebayPricesFilePath = '/usr/home/mipzhm/public_html/EbayPrices.txt';
 //    private $csvFilePath = '/usr/home/mipzhm/public_html/Teileexport-grp-3.csv';
@@ -24,7 +33,7 @@ class DeleteProducts
         $this->dbalConnection = Shopware()->Container()->get('dbal_connection');
     }
 
-    public function deleteProductsFromCsv()
+    public function execute()
     {
         file_put_contents($this->logFilePath, '');
 
@@ -35,7 +44,11 @@ class DeleteProducts
         while ($row = fgetcsv($csvFile, 0, ';')) {
             $rowData = array_combine($headers, $row);
             if (strlen($rowData['external_id']) < 4) {
-                $logMessage = 'Product with ID = ' . $rowData['products_id'] . '; External ID = ' . $rowData['external_id'] . '; Name = ' . $rowData['products_name'] . '; Tax ID = ' . $rowData['products_tax_class_id'] . " was not deleted!\n";
+                $logMessage = 'Product with ID = ' . $rowData['products_id']
+                    . '; External ID = ' . $rowData['external_id']
+                    . '; Name = ' . $rowData['products_name']
+                    . '; Tax ID = ' . $rowData['products_tax_class_id']
+                    . " was not deleted!\n";
                 echo $logMessage;
                 file_put_contents($this->logFilePath, $logMessage, FILE_APPEND);
                 continue;
@@ -56,51 +69,45 @@ class DeleteProducts
 
         file_put_contents($this->ebayPricesFilePath, json_encode($ebayPrices));
 
-        $productData = [];
+        $productIds = [];
         $result = Shopware()->Db()->query("SELECT articleID FROM s_articles_details WHERE ordernumber IN ('" . implode("','", $orderNumbers) . "')");
         foreach ($result as $row) {
-            array_push($productData, array('id' => $row['articleID']));
+            $productIds[] = array('id' => $row['articleID']);
 
-            if (count($productData) >= 500) {
+            if (count($productIds) >= 500) {
                 $time_start = microtime(true);
 
-                $this->deleteProducts($productData);
-                $productData = [];
+                $this->deleteProducts($productIds);
+                $productIds = [];
 
                 $time_end = microtime(true);
                 $execution_time = ($time_end - $time_start);
 
-                echo 'Total execution time for remove 500 products: ' . $execution_time . ' sec';
-                echo "\n";
+                echo 'Total execution time for deleting 500 products: ' . $execution_time . " sec\n";
             }
         }
 
-        if (count($productData) > 0) {
-            $this->deleteProducts($productData);
+        if (count($productIds) > 0) {
+            $this->deleteProducts($productIds);
         }
+
+        echo "Deleting completed\n";
     }
 
-    /**
-     * @param array $productData
-     * @return void
-     */
-    function deleteProducts(array $productData)
+    function deleteProducts(array $productIds)
     {
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $this->endpointUrl . '/articles');
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'DELETE');
-        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($productData));
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($productIds));
         curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
-        curl_setopt($ch, CURLOPT_USERPWD, "$this->userName:$this->apiKey");
+        curl_setopt($ch, CURLOPT_USERPWD, $this->userName . ':' . $this->apiKey);
         curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
         $response = curl_exec($ch);
 
         if (curl_errno($ch)) {
             echo 'Error: ' . curl_error($ch);
-        } else {
-            echo '500 product deleted successfully!';
-            echo "\n";
         }
 
         curl_close($ch);
