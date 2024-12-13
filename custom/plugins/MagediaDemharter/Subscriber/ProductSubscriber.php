259b43af3953c584dd9381133a236ec829c949c6
Vladislav-Matsiushenko <vladislav.matsiushenko@softera.by>
2024-12-13 10:55:07 +0300

    Added product description update

---
+++ custom/plugins/MagediaDemharter/Subscriber/ProductSubscriber.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace MagediaDemharter\Subscriber;
+
+use Enlight\Event\SubscriberInterface;
+use Shopware\Models\Article\Article;
+use Doctrine\ORM\EntityManager;
+
+class ProductSubscriber implements SubscriberInterface
+{
+    const MANUFACTURER_DESCRIPTION = '<DIV><FONT size=2 face=Tahoma></FONT>&nbsp;</DIV><DIV><FONT size=2 face=Tahoma>Informationen zur Produktsicherheit:</FONT></DIV><DIV><FONT size=2 face=Tahoma></FONT>&nbsp;</DIV><DIV><FONT size=2 face=Tahoma>Hersteller/EU-Verantwortlicher:</FONT></DIV><DIV><FONT size=2 face=Tahoma>Quad Stadel Schwab GmbH</FONT></DIV><DIV><FONT size=2 face=Tahoma>Im Herrmannshof 5</FONT></DIV><DIV><FONT size=2 face=Tahoma>91595 Burgoberbach</FONT></DIV><DIV><FONT size=2 face=Tahoma>Telefon: +0049 09805/932550</FONT></DIV>';
+
+    /**
+     * @var EntityManager
+     */
+    private $entityManager;
+
+    public function __construct(EntityManager $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return [
+            'Shopware\Models\Article\Article::postPersist' => 'onProductCreate'
+        ];
+    }
+
+    public function onProductCreate(\Enlight_Event_EventArgs $args)
+    {
+        $entity = $args->getEntity();
+
+        if ($entity instanceof Article) {
+            $descriptionLong = $entity->getDescriptionLong();
+            $entity->setDescriptionLong($descriptionLong . self::MANUFACTURER_DESCRIPTION);
+
+            $this->entityManager->persist($entity);
+            $this->entityManager->flush();
+        }
+    }
+}
