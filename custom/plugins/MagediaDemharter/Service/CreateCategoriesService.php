ec08b892d355d4fc31296fbd030ab9e5ff621303
Vladislav-Matsiushenko <vladislav.matsiushenko@softera.by>
2025-06-11 17:59:02 +0300

    Fixed creating categories for new import

--- custom/plugins/MagediaDemharter/Service/CreateCategoriesService.php
+++ custom/plugins/MagediaDemharter/Service/CreateCategoriesService.php
@@ -5,118 +5,106 @@ namespace MagediaDemharter\Service;
 class CreateCategoriesService
 {
 // Local
-    private $productsDataCsvFilePath = '/var/www/quad-ersatzteile.loc/files/demharter/ProductsData.csv';
-    private $techPartsDataCsvFilePath = '/var/www/quad-ersatzteile.loc/files/demharter/TechPartsData.csv';
-    private $categoriesTreesFilePath = '/var/www/quad-ersatzteile.loc/files/demharter/CategoriesTrees.txt';
+    private $categoryDataCsvFilePath = '/var/www/quad-ersatzteile.loc/files/demharter/CategoryData.csv';
+    private $updatedCategoryDataFilePath = '/var/www/quad-ersatzteile.loc/files/demharter/UpdatedCategoryData.txt';
     private $endpointUrl = 'http://quad-ersatzteile.loc/api';
 
 // Staging
-//    private $productsDataCsvFilePath = '/usr/home/mipzhm/public_html/staging/files/demharter/ProductsData.csv';
-//    private $techPartsDataCsvFilePath = '/usr/home/mipzhm/public_html/staging/files/demharter/TechPartsData.csv';
-//    private $categoriesTreesFilePath = '/usr/home/mipzhm/public_html/staging/files/demharter/CategoriesTrees.txt';
+//    private $categoryDataCsvFilePath = '/usr/home/mipzhm/public_html/staging/files/demharter/CategoryData.csv';
+//    private $updatedCategoryDataFilePath = '/usr/home/mipzhm/public_html/staging/files/demharter/UpdatedCategoryData.txt';
 //    private $endpointUrl = 'http://staging.quad-ersatzteile.com/api';
 
 // Live
-//    private $productsDataCsvFilePath = '/usr/home/mipzhm/public_html/files/demharter/ProductsData.csv';
-//    private $techPartsDataCsvFilePath = '/usr/home/mipzhm/public_html/files/demharter/TechPartsData.csv';
-//    private $categoriesTreesFilePath = '/usr/home/mipzhm/public_html/files/demharter/CategoriesTrees.txt';
+//    private $categoryDataCsvFilePath = '/usr/home/mipzhm/public_html/files/demharter/CategoryData.csv';
+//    private $updatedCategoryDataFilePath = '/usr/home/mipzhm/public_html/files/demharter/UpdatedCategoryData.txt';
 //    private $endpointUrl = 'https://www.quad-ersatzteile.com/api';
     private $categoryName = 'Quad/Scooter spare parts';
     private $userName = 'schwab';
     private $apiKey = 'pdw4kVus56U9IcFaKuHKv7QFQABtKeG20ub5rAh3';
     private $helper;
-    private $modelManager;
-    private $dbalConnection;
 
     public function __construct()
     {
         $this->helper = Shopware()->Container()->get('magedia_demharter.helper');
         ini_set('memory_limit', '-1');
-        $this->modelManager = Shopware()->Container()->get('models');
-        $this->dbalConnection = Shopware()->Container()->get('dbal_connection');
     }
 
     public function execute()
     {
         $startTime = microtime(true);
 
+        $parentCategoryId = 0;
+        $result = Shopware()->Db()->query('SELECT * FROM s_categories WHERE description = :value', [
+            'value' => $this->categoryName
+        ]);
+        foreach ($result as $row) {
+            $parentCategoryId = $row['id'];
+        }
+        if ($parentCategoryId === 0) {
+            echo 'Category ' . $this->categoryName . " does not exist\n";
+            return;
+        }
+
         $categoriesData = [];
-        $csvFile = fopen($this->productsDataCsvFilePath, 'r');
+        $csvFile = fopen($this->categoryDataCsvFilePath, 'r');
         $headers = fgetcsv($csvFile, 0, ';');
         while ($row = fgetcsv($csvFile, 0, ';')) {
             $rowData = array_combine($headers, $row);
-            if ($rowData['products_category_tree'] == '' || $rowData['products_category_tree'] == "Artikel noch nicht zugewiesen") {
-                echo 'Product with ID = ' . $rowData['products_id'] . " has no category\n";
+            if ($rowData['categories_id'] === '' || $rowData['categories_level'] === '' || $rowData['parent_id'] === '' || $rowData['categories_name'] === '' || $rowData['categories_name'] === "Artikel noch nicht zugewiesen") {
+                echo 'Category with ID = ' . $rowData['categories_id'] . " was not created\n";
                 continue;
             }
 
-            $categoriesData[] =  $this->categoryName . ' => ' . $rowData['products_category_tree'];
+            $categoriesData[] =  [
+                'categories_id' => $rowData['categories_id'],
+                'categories_level' => $rowData['categories_level'],
+                'parent_id' => $rowData['parent_id'],
+                'categories_name' => $rowData['categories_name'],
+            ];
         }
         fclose($csvFile);
 
-        $csvFile = fopen($this->techPartsDataCsvFilePath, 'r');
-        $headers = fgetcsv($csvFile, 0, ';');
-        while ($row = fgetcsv($csvFile, 0, ';')) {
-            $rowData = array_combine($headers, $row);
-            if ($rowData['products_category_tree'] == '' || $rowData['products_category_tree'] == "Artikel noch nicht zugewiesen") {
-                echo 'Category with ID = ' . $rowData['categories_id'] . ' linked with product with ID = ' . $rowData['products_id'] . " has no name\n";
-                continue;
+        usort($categoriesData, function ($a, $b) {
+            $levelCompare = $a['categories_level'] <=> $b['categories_level'];
+            if ($levelCompare !== 0) {
+                return $levelCompare;
             }
 
-            $categoriesData[] =  $this->categoryName . ' => ' . $rowData['products_category_tree'];
-        }
-        fclose($csvFile);
-        $categoriesData = array_unique($categoriesData);
-        asort($categoriesData);
+            return strcmp($a['categories_name'], $b['categories_name']);
+        });
 
-        $categories = $this->helper->getCategories($this->endpointUrl, $this->userName, $this->apiKey);
-        $categoriesCount = count($categoriesData);
-        $createdCategoriesCount = 0;
-        foreach ($categoriesData as $names) {
-            $names = explode('=>', $names);
-            $names = array_map('trim', $names);
-
-            $parentCategoryId = 0;
-            $subCategoriesCount = 0;
-            $firstIteration = true;
-            foreach ($names as $name){
-                $found = false;
-                foreach ($categories as $category) {
-                    if ($name == $category->name && ($firstIteration || $category->parentId == $parentCategoryId)) {
-                        $subCategoriesCount++;
-                        $parentCategoryId = $category->id;
-                        $found = true;
-                        break;
-                    }
-                }
-                if (!$found) {
-                    break;
-                }
-                $firstIteration = false;
-            }
-
-            for ($i = $subCategoriesCount; $i < count($names); $i++) {
+        foreach ($categoriesData as &$categoryData) {
+            if ($categoryData['categories_level'] === '1' && $categoryData['parent_id'] === '0') {
                 $response = $this->helper->createCategory($this->endpointUrl, $this->userName, $this->apiKey,
-                    json_encode(array('name' => $names[$i], 'parentId' => $parentCategoryId))
+                    json_encode(array('name' => $categoryData['categories_name'], 'parentId' => $parentCategoryId))
                 );
 
                 $newCategoryId = json_decode($response)->data->id;
                 if ($newCategoryId) {
-                    $categories[] = (Object)['id' => $newCategoryId, 'name' => $names[$i], 'parentId' => $parentCategoryId];
-                    $parentCategoryId = $newCategoryId;
+                    $categoryData['categories_db_id'] = $newCategoryId;
+                    $categoryData['categories_level'] = null;
+                    $categoryData['parent_id'] = null;
+                }
+            } else {
+                foreach ($categoriesData as $categoryDataParent) {
+                    if ($categoryData['parent_id'] === $categoryDataParent['categories_id']) {
+                        $response = $this->helper->createCategory($this->endpointUrl, $this->userName, $this->apiKey,
+                            json_encode(array('name' => $categoryData['categories_name'], 'parentId' => $categoryDataParent['categories_db_id']))
+                        );
+
+                        $newCategoryId = json_decode($response)->data->id;
+                        if ($newCategoryId) {
+                            $categoryData['categories_db_id'] = $newCategoryId;
+                            $categoryData['categories_level'] = null;
+                            $categoryData['parent_id'] = null;
+                        }
+                        break;
+                    }
                 }
             }
-
-            $createdCategoriesCount++;
-            if ($createdCategoriesCount % 500 == 0) {
-                echo 'Created ' . $createdCategoriesCount . ' categories. ' . ($categoriesCount - $createdCategoriesCount) . " left\n";
-            }
         }
-        unset($categories);
-        unset($categoriesData);
 
-        $categoriesTrees = $this->helper->getCategoriesTrees($this->endpointUrl, $this->userName, $this->apiKey, $this->categoryName);
-        file_put_contents($this->categoriesTreesFilePath, json_encode($categoriesTrees));
+        file_put_contents($this->updatedCategoryDataFilePath, json_encode($categoriesData));
 
         $executionTime = (microtime(true) - $startTime);
         echo 'Creating categories completed in ' . $executionTime . " seconds\n";
